a1a07d0e39eaf359f9eaf5f370e667933023acc4
Mathieu Baudier <mbaudier@argeo.org>
2023-03-16 09:00:00 +0100

    Fix ECJ branch

--- common.mk
+++ common.mk
@@ -2,15 +2,6 @@ build-major=2
 build-minor=3
 
 # Required third party libraries
-ECJ_BRANCH=4.26
+ECJ_BRANCH=3.32
 BNDLIB_BRANCH=5.3
 SYSLOGGER_BRANCH=$(build-major).$(build-minor)
-
-#A2_BASE=/home/mbaudier/dev/git/unstable/output/a2 /home/mbaudier/dev/git/unstable/argeo-qa/build/argeo/output/a2 test
-
-find-build-tp:
-	$(lastword $(foreach base, $(A2_BASE), $(wildcard $(base)/org.argeo.tp/org.argeo.tp.syslogger.$(SYSLOGGER_BRANCH).jar)))
-	##$(eval LOGGER_JAR = $(shell if [ -f $(base)/org.argeo.tp/org.argeo.tp.syslogger.$(SYSLOGGER_BRANCH).jar ]; then echo $(base)/org.argeo.tp/org.argeo.tp.syslogger.$(SYSLOGGER_BRANCH).jar; fi)) \
-	#)
-#	$(if $(wildcard $(candidate)), $(eval LOGGER_JAR = $(candidate))) \	
-	echo $(LOGGER_JAR)
