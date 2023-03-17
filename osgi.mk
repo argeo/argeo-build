f0be3f409525a91bcd2d7eaf003bcd7621716225
Mathieu Baudier <mbaudier@argeo.org>
2023-03-17 05:31:26 +0100

    Fix conditional in Make

--- osgi.mk
+++ osgi.mk
@@ -66,7 +66,7 @@ $(BUILD_BASE)/%/to-build : $$(shell find % -type f -not -path 'bin/*' -not -path
 
 # Local manifests
 %/META-INF/MANIFEST.MF : $(BUILD_BASE)/%/META-INF/MANIFEST.MF
-ifneq($(NO_MANIFEST_COPY),true)
+ifneq ($(NO_MANIFEST_COPY),true)
 	@mkdir -p $*/META-INF
 	@cp $< $@
 endif
