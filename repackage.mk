042b45bd86c0ccbf66f6083f7073c76f985560f5
Mathieu Baudier <mbaudier@argeo.org>
2024-03-06 07:36:39 +0100

    Improve native install

--- repackage.mk
+++ repackage.mk
@@ -31,12 +31,14 @@ all: $(BUILD_BASE)/repackaged
 
 install:
 	@$(foreach category, $(PORTABLE_CATEGORIES), mkdir -p $(A2_INSTALL_TARGET)/$(category);  cp $(A2_OUTPUT)/$(category)/*.jar $(A2_INSTALL_TARGET)/$(category);)
-	@echo Installed $(PORTABLE_CATEGORIES) to $(A2_INSTALL_TARGET)
+	@echo Installed portable jars '$(PORTABLE_CATEGORIES)' to $(A2_INSTALL_TARGET)
 	@$(foreach category, $(OS_CATEGORIES), mkdir -p $(A2_INSTALL_TARGET)/$(category);  cp $(A2_OUTPUT)/$(category)/*.jar $(A2_INSTALL_TARGET)/$(category);)
-	@echo Installed $(OS_CATEGORIES) to $(A2_INSTALL_TARGET)
+	@echo Installed OS-dependent jars '$(OS_CATEGORIES)' to $(A2_INSTALL_TARGET)
 	mkdir -p $(A2_NATIVE_INSTALL_TARGET)
+	@$(foreach category, $(ARCH_CATEGORIES), mkdir -p $(A2_NATIVE_INSTALL_TARGET)/$(category);  cp $(A2_OUTPUT)/$(category)/*.jar $(A2_NATIVE_INSTALL_TARGET)/$(category);)
+	@echo Installed arch-dependent jars '$(ARCH_CATEGORIES)' to $(A2_NATIVE_INSTALL_TARGET)
 	@$(foreach category, $(ARCH_CATEGORIES), cp $(A2_OUTPUT)/$(category)/*.so $(A2_NATIVE_INSTALL_TARGET);)
-	@echo Installed $(ARCH_CATEGORIES) to $(A2_NATIVE_INSTALL_TARGET)
+	@echo Installed arch binaries '$(ARCH_CATEGORIES)' to $(A2_NATIVE_INSTALL_TARGET)
 
 uninstall:
 	@$(foreach category, $(CATEGORIES), rm -rf $(A2_INSTALL_TARGET)/$(category);)
